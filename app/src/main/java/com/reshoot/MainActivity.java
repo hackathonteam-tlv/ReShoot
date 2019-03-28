05fff2f88bf8939fb53b99106dd145e035e7e08c
Sergey Medgaus <sergey.medgaus@gmail.com>
2019-03-28 20:05:40 +0200

    Setting full screen to the main activity

--- app/src/main/java/com/reshoot/MainActivity.java
+++ app/src/main/java/com/reshoot/MainActivity.java
@@ -24,6 +24,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.cameraview.AspectRatio;
@@ -138,6 +140,11 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // setting full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
